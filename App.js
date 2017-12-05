e6561213b9187d4cd95a8306255e202a36e9189e
Francisco Magalhaes <franciscomags@gmail.com>
2017-12-05 15:54:57 +0000

    Added search feature

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import Search from './screens/Search';
 import Saved from './screens/Saved';
+import search from './api';
 
 export const Navigator = TabNavigator(
   {
@@ -36,7 +37,30 @@ export const Navigator = TabNavigator(
 );
 
 export default class App extends React.Component {
+  state = { searchResults: [], q: '' };
+
+  search = async () => {
+    const response = await search(this.state.q);
+    const body = await response.json();
+    const searchResults = body.data.map(ele => ele.images.downsized.url); 
+
+    this.setState({ searchResults });
+  };
+
+  updateQuery = text => this.setState({ q: text });
+
   render() {
-    return <Navigator />;
+    return (
+      <Navigator
+        screenProps={{
+          Search: {
+            search: this.search,
+            updateQuery: this.updateQuery,
+            searchResults: this.state.searchResults,
+            query: this.state.q,
+          },
+        }}
+      />
+    );
   }
 }
