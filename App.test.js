efae9f9b561d3441e40f3969a57e34ac841b9f2a
Tony Gonçalves <tonyfg.pt@gmail.com>
2017-12-08 14:20:04 +0000

    Revert "[0] Navigation setup & customization"

--- App.test.js
+++ App.test.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import Enzyme, { shallow, mount, render } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
-import Navigator from './Navigator';
-import Saved from './screens/Saved';
 
-Enzyme.configure({ adapter: new Adapter() });
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
-});
-
-it('renders main navigator', () => {
-  const wrapper = shallow(<App />);
-
-  expect(wrapper.find(Navigator).length).toBe(1);
 });
